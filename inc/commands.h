02cb5bd9b36961c03f2aada631ff24f794993357
c.b. <98446093+dloiolb@users.noreply.github.com>
2025-03-26 14:23:13 -0500

    fixed header formattings

--- inc/commands.h
+++ inc/commands.h
@@ -1,10 +1,18 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+// include files:
+
 #include "ed.h"
+
+// external data types and variables:
 
 typedef struct Buffer Buffer;
 extern Buffer buffer; 
+
+// NODE_H data types:
+
+// NODE_H functions:
 
 int command_E();
 
--- inc/ed.h
+++ inc/ed.h
@@ -1,12 +1,20 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+// include files:
+
 #include "node.h"
+
+// external data types and variables:
+
+// COMMANDS_H data types:
 
 typedef struct Buffer {
     Node * head; // pointer to the first line (line 1)
     Node * tail; // pointer to the last line (line line_count)
     int line_count; // number of lines in buffer
 } Buffer;  
+
+// COMMANDS_H functions:
 
 #endif
